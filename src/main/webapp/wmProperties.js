91bbc6ee4e5f196047ec2d5e44a5ca6b84067a1a
root <root@76e6cc14dc4a>
2017-06-22 07:28:39 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_cDNyUazwQb",
+  "homePage" : "Main",
+  "name" : "Auto_cDNyUazwQb",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
